033c663e274051f70758f3e731474dda8ab18028
Joberne <joberneneb@gmail.com>
2025-07-03 00:28:38 +0200

    Update Route.php

--- router/Route.php
+++ router/Route.php
@@ -1,28 +1,24 @@
-<?php 
+<?php
+declare(strict_types=1);
 
-/**
- * @author  Bedel Ngambé Ebouato <joberneneb@gmail.com>
- * @package Delices\Router
- */
+namespace App\Core\Routing;
+use App\Container\ContainerInterface;
+use App\Core\Routing\Exceptions\RouteNotFoundException;
+use App\Controllers\Users;
 
-declare(strict_types=1); 
-
-namespace Delices\Router;
-
-use       Delices\Container\ContainerInterface;
-
-class Route {
-	/**
-	 * Regex pattern to match url
-	 */
-	private const PATTERN = '#{([\w]+)}#';
+class Route
+{
     /**
-	 *
+     * Regex pattern to match url
+     */
+     private const PATTERN = '#{([\w]+)}#';
+    /**
+     *
      * @var ContainerInterface $container
      */
     public ContainerInterface $container;
     /**
-	 *
+     *
      * @var string $namespace Controller namespace
      */
     public string $namespace = '';
@@ -43,180 +39,123 @@ class Route {
      */
     private array $matches = [];
     /**
-	 *
+     *
      * @var string $path URL path
      */
     private string $path;
     /**
-	 *
+     *
      * @var callable $callable Callable to run later
      */
     private $callable;
     /**
-	 *
      * Constructor 
      * 
      * @param string $path
      * @param mixed  $callable
-	 *
+     *
      * @return void
      */
     public function __construct(string $path, $callable) 
     {
-        $this -> path = trim($path, '/');
-        $this -> callable = $callable;
+        $this->path = trim($path, '/');
+        $this->callable = $callable;
     }
     /**
-     * Match url with the path
+     * Set the parameters for the route
+     * @param string $url
      * 
-     * @param  string $url URL
-	 *
-     * @access public
-	 *
-     * @return bool
+     * @return void
      */
     public function match(string $url): bool 
-	{
+    {
         $url = trim($url, '/');
-        $path = preg_replace_callback(self::PATTERN, [$this, 'paramMatch'], $this -> path); 
+        $path = preg_replace_callback(self::PATTERN, [$this, 'paramMatch'], $this->path); 
         $regex = "#^$path$#i";
 
         if (!preg_match($regex, $url, $matches)) {
-
             return false;
-
         }
-
         array_shift($matches); 
-        
-        $this -> matches = $matches;
-        
-        $this -> namedParams = $this -> paramNames($matches);
-
+        $this->matches = $matches;
+        $this->namedParams = $this->paramNames($matches);
         return true;
-
     }
     /**
-     * Constraint the url parameters
+     * Get the named parameters
+     * @param array $values Values to set for the named parameters
      * 
-     * @param  string $param URL parameter
-     * @param  string $regex Regular expression - defines the parameter constraint
-	 *
-     * @access public
-	 *
-     * @return self
-     */
-    public function with(string $param, string $regex): self 
-	{
-        $this -> params[$param] = str_replace('(', '(?:',$regex); 
-
-        return $this;
-    }
-    /**
-     * Execute the callable
-     * 
-     * @access public
-	 *
-     * @return mixed
-     */
-    public function call() 
-    {   
-        $this -> container -> set($this -> namedParams);
-
-        $resolver  = new MethodResolver($this -> container);
-    
-        $resolver -> AddNamespace($this -> namespace);
-       
-        if (is_string($this -> callable)) {
-
-            return $resolver -> resolve($this -> callable);
-            
-        } 
-        
-        if (is_callable($this -> callable)) {
-
-            return call_user_func_array($this -> callable, $this -> matches);
-            
-        } 
-        
-        if (is_array($this -> callable)) {
-            
-            $callable = implode('#', $this -> callable);
-
-            return $resolver -> resolve($callable, $this -> namedParams);
-        }
-
-    }
-    /**
-     * Add controller namespace
-     * 
-     * @param  string $namespace Controller namespace
-	 *
-     * @access public 
-	 *
-     * @return self
-     */
-    public function AddNamespace(string $namespace): self
-    {
-        $this -> namespace = $namespace;
-
-        return $this;
-    }
-    /**
-     * Add container of injection
-     * 
-     * @param  ContainerInterface $container
-	 *
-     * @access public
-	 *
-     * @return self
-     */
-    public function addContainer(ContainerInterface $container): self
-    {
-        $this -> container = $container;
-
-        return $this;
-    }
-    /**
-     * Match parameter with the regex constraint
-     * 
-     * @param  array   $match
-	 *
-     * @access private
-	 *
-     * @return string 
-     */
-    private function paramMatch(array $match): string 
-    {
-        if (isset($this -> params[$match[1]])) {
-            
-            return '('.$this -> params[$match[1]].')';
-        }
-
-        return '([^/]+)';
-    }
-     /**
-     * Get named parameters
-     * 
-     * @param  array<string,mixed> $values Array containing pairs/values
-	 *
-     * @access private
-	 *
-     * @return array<string,mixed>         Value/pair array of parameters
+     * @return array<string,mixed>
      */
     private function paramNames(array $values = []): array 
     {
         $keys = [];
-        
-        preg_match_all(self::PATTERN, $this -> path, $matches, PREG_SET_ORDER);
+        preg_match_all(self::PATTERN, $this->path, $matches, PREG_SET_ORDER);
     
-        foreach($matches as $match) {
-            
+        foreach($matches as $match) {  
             $keys[] = $match[1];
-
-        }
-       
+        } 
         return array_combine($keys, $values);
     }
-   
+    /**
+     * Get the named parameters
+     * @param array $match Match array from preg_match
+     * 
+     * @return string 
+     */
+    private function paramMatch(array $match): string 
+    {
+        if (isset($this->params[$match[1]])) {
+            return '('.$this->params[$match[1]].')';
+        }
+        return '([^/]+)';
+    }
+    /**
+     * Excute the callable with the matched parameters
+     * 
+     * @return array<string,mixed>
+     */
+    public function call() 
+    {   
+        if (is_callable($this->callable)) {
+            return call_user_func_array($this->callable, $this->matches);
+        }  
+        if (is_string($this->callable)) {
+            $parts = explode('#', $this->callable);
+            $controller = $this->namespace.'\\'.ucfirst($parts[0]);
+            $methode = $parts[1];
+            
+            if (!class_exists($controller)) {
+                throw new \RuntimeException("Controller class $controller does not exist.");
+            }
+            $controllerReflection = new \ReflectionClass($controller);
+            $instance = isset($this->container) ? $this->container->get($controller) : $controllerReflection->newInstance();
+            $methodeReflection = $controllerReflection->getMethod($methode);
+            $args = isset($this->container) 
+            ? $this->container->resolveMethodParameters($methodeReflection, $this->matches) : [];
+
+            echo $methodeReflection->invokeArgs($instance, $args);
+        }
+    }
+    /**
+     * Set the namespace for the controller
+     * @param string $namespace
+     * 
+     * @return void
+     */
+    public function setNamespace(string $namespace): void 
+    {
+        $this->namespace = $namespace;
+    }
+    /**
+     * Set the container for dependency injection
+     *
+     * @param ContainerInterface $container
+     *
+     * @return void
+     */
+    public function setContainer(ContainerInterface $container): void 
+    {
+        $this->container = $container;
+    }
 }
